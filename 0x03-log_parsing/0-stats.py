778dd29b2b58dd20941a90a05eb56b4a6dfb8c65
fabrizia2 <melvinrenish56@gmail.com>
2023-05-11 07:05:47 +0300

    a script that reads stdin line by line and computes metrics

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,59 +1,47 @@
 #!/usr/bin/python3
 
-"""reads stdin line by line and computes metrics"""
+"""Script that reads stdin line by line and computes metrics"""
 
 import sys
-import signal
 
 
-# Initialize variables
+def print_status(code_dict, total_size):
+    """Prints the computed metrics"""
+    print("Total file size: {:d}".format(total_size))
+    for code in sorted(code_dict.keys()):
+        if code_dict[code] != 0:
+            print("{}: {:d}".format(code, code_dict[code]))
+
+
+status_codes = {"200": 0, "301": 0, "400": 0,
+                "401": 0, "403": 0, "404": 0, "405": 0, "500": 0}
+
+count = 0
 total_size = 0
-status_codes = {}
 
+try:
+    for line in sys.stdin:
+        if count != 0 and count % 10 == 0:
+            print_status(status_codes, total_size)
 
-# Define a signal handler for KeyboardInterrupt
-def signal_handler(signal, frame):
-    """signal handler"""
-    print_statistics()
-    sys.exit(0)
+        line_parts = line.split()
+        count += 1
 
+        try:
+            file_size = int(line_parts[-1])
+            total_size += file_size
+        except Exception:
+            pass
 
-# Function to print statistics
-def print_statistics():
-    """print statistics"""
-    print("Total file size:", total_size)
-    for code in sorted(status_codes.keys()):
-        print(code, ":", status_codes[code])
+        try:
+            status_code = line_parts[-2]
+            if status_code in status_codes:
+                status_codes[status_code] += 1
+        except Exception:
+            pass
 
+    print_status(status_codes, total_size)
 
-# Register the signal handler for KeyboardInterrupt
-signal.signal(signal.SIGINT, signal_handler)
-
-# Read input lines
-for line_number, line in enumerate(sys.stdin, start=1):
-    line = line.strip()
-
-    # Parse the line
-    parts = line.split()
-    if len(parts) < 7 or parts[6] not in
-    ('200', '301', '400', '401', '403', '404', '405', '500'):
-        continue
-
-    try:
-        file_size = int(parts[7])
-    except ValueError:
-        continue
-
-    # Update total file size
-    total_size += file_size
-
-    # Update status code count
-    status_code = parts[6]
-    status_codes[status_code] = status_codes.get(status_code, 0) + 1
-
-    # Print statistics every 10 lines
-    if line_number % 10 == 0:
-        print_statistics()
-
-# Print final statistics
-print_statistics()
+except KeyboardInterrupt:
+    print_status(status_codes, total_size)
+    raise
