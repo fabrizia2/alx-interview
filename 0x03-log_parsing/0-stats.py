3fdb0adbc2ceb64b9393888839e75ea9a511c45f
fabrizia2 <melvinrenish56@gmail.com>
2023-05-11 07:31:48 +0300

    a script that reads stdin line by line and computes metrics

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -3,52 +3,56 @@
 """reads stdin line by line and computes metrics"""
 
 import sys
-import re
+import signal
+
+# Initialize variables
+total_size = 0
+status_codes = {}
 
 
-def main():
-    """reads stdin line by line and computes metrics"""
-    current_dict = {}
-    for num in [200, 301, 400, 401, 403, 404, 405, 500]:
-        current_dict[str(num)] = 0
-
-    file_size = 0
-    count = 0
-
-    while True:
-        line = sys.stdin.readline()
-        if not line:
-            break
-
-        st = r'\[(.*?)\] "GET \/projects\/260 HTTP\/1\.1" (\d{3}) (\d+)$'
-        pattern = r'^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) - ' + st
-
-        match = re.match(pattern, line)
-        if not match:
-            continue
-
-        status_code = str(match.group(3))
-
-        if status_code in current_dict:
-            count += 1
-            current_dict[status_code] += 1
-            file_size += int(match.group(4))
-
-        try:
-            if count == 10:
-                print("File size: {}".format(file_size))
-                sorted_dict = sorted(current_dict.items(),
-                                     key=lambda x: x)
-                for key, value in sorted_dict:
-                    print("{}: {}".format(key, value))
-                count = 0
-        except KeyboardInterrupt as e:
-            sorted_dict = sorted(current_dict.items(),
-                                 key=lambda x: x)
-            for key, value in sorted_dict:
-                print("{}: {}".format(key, value))
-            print(e)
+# Define a signal handler for KeyboardInterrupt
+def signal_handler(signal, frame):
+    """signal handler for KeyboardInterrupt"""
+    print_statistics()
+    sys.exit(0)
 
 
-if __name__ == "__main__":
-    main()
+# Function to print statistics
+def print_statistics():
+    """Function to print statistics"""
+    print("Total file size:", total_size)
+    for code in sorted(status_codes.keys()):
+        print(code, ":", status_codes[code])
+
+
+# Register the signal handler for KeyboardInterrupt
+signal.signal(signal.SIGINT, signal_handler)
+
+# Read input lines
+for line_number, line in enumerate(sys.stdin, start=1):
+    line = line.strip()
+
+    # Parse the line
+    parts = line.split()
+    if len(parts) < 7 or parts[6] not in
+    ('200', '301', '400', '401', '403', '404', '405', '500'):
+        continue
+
+    try:
+        file_size = int(parts[7])
+    except ValueError:
+        continue
+
+    # Update total file size
+    total_size += file_size
+
+    # Update status code count
+    status_code = parts[6]
+    status_codes[status_code] = status_codes.get(status_code, 0) + 1
+
+    # Print statistics every 10 lines
+    if line_number % 10 == 0:
+        print_statistics()
+
+# Print final statistics
+print_statistics()
