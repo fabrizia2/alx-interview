f913d69499f1a37aa6ca1c2d1b3c380710c95269
fabrizia2 <melvinrenish56@gmail.com>
2023-05-11 19:14:03 +0300

    log_parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -14,7 +14,7 @@ def print_status(code_counts, file_size):
 
 
 status_codes = {"200": 0, "301": 0, "400": 0, "401": 0,
-"403": 0, "404": 0, "405": 0, "500": 0}
+                "403": 0, "404": 0, "405": 0, "500": 0}
 
 line_count = 0
 file_size = 0
