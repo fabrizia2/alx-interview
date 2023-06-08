fa97675e295417425835dc5cfa11f8e7f53067dc
fabrizia2 <melvinrenish56@gmail.com>
2023-06-08 21:49:48 +0300

    Rotate 2D Matrix

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -13,11 +13,11 @@ def rotate_2d_matrix(matrix):
             # save topleft  value
             topLeft = matrix[top][left + i]
             # move bottom left to top left
-            matrix[top][left + i] = matrix[bottom - i][left]       
+            matrix[top][left + i] = matrix[bottom - i][left]
             # move bottom right to bottom left
-            matrix[bottom - i][left] = matrix[bottom][right - i]   
+            matrix[bottom - i][left] = matrix[bottom][right - i]
             # move top right to bottom right
-            matrix[bottom][right - i] = matrix[top + i][right]     
+            matrix[bottom][right - i] = matrix[top + i][right]
             # move top left to top right
             matrix[top + i][right] = topLeft
         right -= 1
